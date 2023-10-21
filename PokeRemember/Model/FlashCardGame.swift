c2c17254ff7a86f4da83d7f807bd4d3a1c8102e5
DavidSpartan95 <112651588+DavidSpartan95@users.noreply.github.com>
2023-10-21 16:26:16 +0200

    Flash Card Game 1.0 complete
    Flash card game now fully works, added all pokemons (until gen 8) in pokeArray

--- PokeRemember/Model/FlashCardGame.swift
+++ PokeRemember/Model/FlashCardGame.swift
@@ -8,44 +8,67 @@
 import Foundation
 
 class FlashCardGame: ObservableObject{
-    var deck = pokeArray
+    @Published var cardsLeft: Int
+    @Published var deck: [PokemonEntry]
     var randomNumber = Int.random(in: 0 ..< pokeArray.count)
-    //@Published var correctAwnser: String
-    var score = 0
+    @Published var score = 0
     @Published var choices: [String]
     
     init() {
         //self.randomNumber = randomNumber
+        self.deck = pokeArray
         self.choices =  [
-            deck[randomNumber].name,
-            deck[Int.random(in: 0 ..< deck.count)].name,
-            deck[Int.random(in: 0 ..< deck.count)].name,
-            deck[Int.random(in: 0 ..< deck.count)].name
+            pokeArray[randomNumber].name,
+            pokeArray[Int.random(in: 0 ..< pokeArray.count)].name,
+            pokeArray[Int.random(in: 0 ..< pokeArray.count)].name,
+            pokeArray[Int.random(in: 0 ..< pokeArray.count)].name
         ]
+        self.cardsLeft = pokeArray.count
         choices.shuffle()
     }
     
     func checkAwnser(awsner:String){
-        
-        if awsner == pokeArray[randomNumber].name {
+        if awsner == deck[randomNumber].name {
             addPoints()
         }
-        shuffleDeck()
+        shuffleDeck(removeCard: deck[randomNumber].name)
     }
     
     private func addPoints(){
         score += 1
     }
     
-    private func shuffleDeck(){
-        //TODO FILTER OUT THE CARD JUST SHOWN
-        randomNumber = Int.random(in: 0 ..< deck.count)
-        choices =  [
-            deck[randomNumber].name,
-            deck[Int.random(in: 0 ..< deck.count)].name,
-            deck[Int.random(in: 0 ..< deck.count)].name,
-            deck[Int.random(in: 0 ..< deck.count)].name
-        ]
+    private func shuffleDeck(removeCard: String) {
+        
+        // Remove the card that was just shown
+        deck = deck.filter { $0.name != removeCard }
+        if deck.isEmpty {
+            cardsLeft = 0
+            return
+            
+        }
+        cardsLeft = deck.count
+        
+        
+        if deck.count > 0 {
+            randomNumber = Int.random(in: 0 ..< deck.count)
+        } else {
+            randomNumber = 0
+        }
+        
+        var tempChoices: [String] = []
+        //append the correct awnser
+        tempChoices.append(deck[randomNumber].name)
+        //append 3 radnom names from of all pokemon (all must be unique)
+        while tempChoices.count < 4 {
+            
+            let randomName = pokeArray.randomElement()?.name
+            if !tempChoices.contains(where: {$0 == randomName}) {
+                tempChoices.append(randomName ?? "???")
+            }
+        }
+        
+        choices = tempChoices
         choices.shuffle()
     }
 }
--- PokeRemember/View/PokemonFlashCardVeiw.swift
+++ PokeRemember/View/PokemonFlashCardVeiw.swift
@@ -16,38 +16,44 @@ struct PokemonFlashCardView: View {
     var body: some View {
         
         VStack{
+            Text("CARDS LEFT: \(game.cardsLeft)")
             Text("SCORE: \(game.score)")
-            AsyncImage(url: URL(string:game.deck[game.randomNumber].urlPicture)){ phase in
-                if let image = phase.image{
-                    image.resizable().frame(width: 200, height: 200)
-                } else if phase.error != nil {
-                    // Handle error
-                    Text("Error loading image")
-                } else {
-                    // Placeholder or loading view
-                    ProgressView()
+            if game.deck.isEmpty {
+                Image(systemName: "checkmark.circle")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.green)
+            }else {
+                AsyncImage(url: URL(string:game.deck[game.randomNumber].urlPicture)){ phase in
+                    if let image = phase.image{
+                        image.resizable().frame(width: 200, height: 200)
+                    } else if phase.error != nil {
+                        // Handle error
+                        Text("Error loading image")
+                    } else {
+                        // Placeholder or loading view
+                        ProgressView()
+                    }
+                }
+                HStack{
+                    Button(game.choices[0], action: {
+                        game.checkAwnser(awsner: game.choices[0])
+                        
+                    })
+                    Button(game.choices[1], action: {
+                        game.checkAwnser(awsner: game.choices[1])
+                    })
+                }
+                HStack{
+                    Button(game.choices[2], action: {
+                        game.checkAwnser(awsner: game.choices[2])
+                    })
+                    Button(game.choices[3], action: {
+                        game.checkAwnser(awsner: game.choices[3])
+                    })
                 }
             }
-            HStack{
-                Button(game.choices[0], action: {
-                    game.checkAwnser(awsner: game.choices[0])
-                    
-                })
-                Button(game.choices[1], action: {
-                    game.checkAwnser(awsner: game.choices[1])
-                })
-            }
-            HStack{
-                Button(game.choices[2], action: {
-                    game.checkAwnser(awsner: game.choices[2])
-                })
-                Button(game.choices[3], action: {
-                    game.checkAwnser(awsner: game.choices[3])
-                })
-            }
         }
-        
-        
     }
     
 }
